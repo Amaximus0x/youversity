e6cafd3a0f94c7a23fe5ec48944b698027b860d0
Amaximus0x <derevyan@gmail.com>
2025-01-06 02:47:51 +0600

    Vercel deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,11 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-        adapter: adapter()
-    },
-	// kit: {
-	// 	adapter: adapter({
-	// 		// Custom configuration for Replit
-	// 		out: 'build',
-	// 		precompress: false,
-	// 		envPrefix: ''
-	// 	}),
-	// 	// Add this csrf configuration
-	// 	csrf: {
-	// 		checkOrigin: false
-	// 	}
-	// },
-
+		adapter: adapter()
+	},
 	preprocess: vitePreprocess()
 };
 
