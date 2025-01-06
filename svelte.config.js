773e7049ffbf7fd52f249f0b309c298d114aeb64
Amaximus0x <derevyan@gmail.com>
2025-01-06 12:22:41 +0600

    Added replit and Vercel configuration

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,24 @@
-import adapter from '@sveltejs/adapter-vercel';
+import vercelAdapter from '@sveltejs/adapter-vercel';
+import nodeAdapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const DEPLOYMENT_PLATFORM = process.env.DEPLOYMENT_PLATFORM || 'vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			runtime: 'nodejs20.x'
-		})
+		adapter: DEPLOYMENT_PLATFORM === 'replit' 
+			? nodeAdapter({
+					out: 'build',
+					precompress: false,
+					envPrefix: ''
+				})
+			: vercelAdapter({
+					runtime: 'nodejs20.x'
+				}),
+		csrf: {
+			checkOrigin: false
+		}
 	},
 	preprocess: vitePreprocess()
 };
