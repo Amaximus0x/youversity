df59c77c640997817ee90f8b640b5c58c7feae00
Amaximus0x <derevyan@gmail.com>
2025-01-10 01:24:21 +0600

    Updated transcript fetching URL

--- src/routes/api/video-transcript/+server.ts
+++ src/routes/api/video-transcript/+server.ts
@@ -6,7 +6,7 @@ async function fetchTranscriptFromYoutube(videoId: string) {
   console.log(`Video ID: ${videoId}`);
   
   try {
-    const response = await axios.get(`https://youtube.com/watch?v=${videoId}`, {
+    const response = await axios.get(`https://www.youtube.com/embed/${videoId}`, {
       headers: {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
         'Accept-Language': 'en-US,en;q=0.9',
